2aaaf1c96c7340bcc3947225b99a5d42a7dedec9
Thiago Ferreira <us.thiago@gmail.com>
2014-05-25 13:02:24 -0300

    Login Screen
    Added login screen.

--- peerassessment/peerassessment/settings.py
+++ peerassessment/peerassessment/settings.py
@@ -78,5 +78,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(BASE_DIR, 'staticfiles'),
+)
--- peerassessment/peerassessment/urls.py
+++ peerassessment/peerassessment/urls.py
@@ -1,7 +1,12 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
 import app
 
 urlpatterns = patterns('',
     # Place routes here
     url(r'^$', 'app.views.index'),
-)
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
