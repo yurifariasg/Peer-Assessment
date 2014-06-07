1efcc03bf13533ad8a50656a90f383778f9358af
Thiago Ferreira <us.thiago@gmail.com>
2014-06-07 17:39:11 -0300

    Authentication working. Login page finished.

--- peerassessment/app/static/js/index.js
+++ peerassessment/app/static/js/index.js
@@ -1,16 +1,24 @@
 
 function login() {
-	var email = $("#email_field").val();
-	var password = $("#password_field").val();
-
+	var email = $("#email").val();
+	var password = $("#password").val();
+	
 	var content = JSON.stringify({ "email" : email, "password" : password });
-
+	console.log("fail: asdas");
 	$.post("login", content,
 	 function( data, txtStatus, xhr ) {
 		// Sucessful!
 		window.location = JSON.parse(data).url;
 	})
 	.fail(function(data, txtStatus, xhr) {
+		$('.alert').show();
 		console.log("fail: " + data.responseText);
+		json_body = JSON.parse(data.responseText);
+		if (json_body.error == "invalid login.") {
+			$("#msg").text("Login/Senha inválido(s)");	
+		} else {
+			$("#msg").text(json_body.error);	
+		}
+		
 	});
 }
